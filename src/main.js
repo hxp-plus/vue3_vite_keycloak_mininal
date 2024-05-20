a41129fe5a4a750aa1018e37dbe8b5358ebfb667
Xiping Hu <hxp@hxp.plus>
2024-05-20 06:41:54 +0000

    引入element-plus

--- src/main.js
+++ src/main.js
@@ -16,4 +16,15 @@ app.mixin(mixin);
 import { filters } from './utils/filters';
 app.config.globalProperties.$filters = filters;
 
+// 引入element-plus
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+app.use(ElementPlus);
+
+// 注册所有图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+
 app.mount('#app');
--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -1,4 +1,16 @@
 <template>
   <div>Hello</div>
   <h1>{{ $filters.sexName(1) }}</h1>
+  <el-button @click="handleClick">submit</el-button>
+  <el-icon><Edit /></el-icon>
 </template>
+
+<script setup>
+import { getCurrentInstance } from 'vue';
+const { proxy } = getCurrentInstance();
+
+async function handleClick() {
+  proxy.submitOk('ABC');
+  proxy.submitFail('提交失败');
+}
+</script>
