22b0bdc209ca4ab07edeadb248b87476d03e616e
Xiping Hu <hxp@hxp.plus>
2024-05-20 07:12:00 +0000

    pinia持久化存储

--- src/main.js
+++ src/main.js
@@ -27,10 +27,29 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component);
 }
 
-// 引入pinia和store
+// pinia
 import { createPinia } from 'pinia';
 const pinia = createPinia();
+
+// 持久化存储
+import { createPersistedState } from 'pinia-plugin-persistedstate';
+pinia.use(
+  createPersistedState({
+    auto: true, // 启用所有 Store 默认持久化
+  }),
+);
+
+// 重写 $reset 方法 => 解决组合式api中无法使用问题
+pinia.use(({ store }) => {
+  const initialState = JSON.parse(JSON.stringify(store.$state));
+  store.$reset = () => {
+    store.$patch(initialState);
+  };
+});
+
 app.use(pinia);
+
+// store
 import store from '@/store';
 app.config.globalProperties.$store = store;
 
